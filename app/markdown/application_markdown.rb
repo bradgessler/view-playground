bc2f6bc2234f87fa43a6c7d733cc9c9ca20bf650
Brad Gessler <bradgessler@gmail.com>
2022-09-24 01:45:21 -0700

    Clean up render code

--- app/markdown/application_markdown.rb
+++ app/markdown/application_markdown.rb
@@ -6,22 +6,16 @@ class ApplicationMarkdown < BaseMarkdown
   end
 
   def block_code(code, language)
-    render_component Views::CodeBlock.new(code, syntax: language)
+    Views::CodeBlock.new(code, syntax: language).call
   end
 
   def image(link, title, alt)
     url = URI(link)
     case url.host
     when "www.youtube.com"
-      render_component Views::YoutubeEmbed.new(url)
+      Views::YoutubeEmbed.new(url).call
     else
       ApplicationController.helpers.image_url link, title: title, alt: alt
     end
-  end
-
-  private
-
-  def render_component(component)
-    component.call("")
   end
 end
